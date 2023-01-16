4858d57131827f85e3a5cfe0c8feecabe24aaeb0
alaliyo <des321321@naver.com>
2023-01-16 20:03:32 +0900

    feat: useEffect 2

--- react-study-start/src/App.js
+++ react-study-start/src/App.js
@@ -2,17 +2,22 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setValue((prev) => prev+1);
+  const onChange = (event) => setKeyword(event.target.value);
 
-  console.log('계속 실행');
-
- 
   useEffect(() => {
-    console.log("api 호출");
-  }, []);
+    console.log('계속 실행');
+  }, [counter]);
+
+  useEffect(() => console.log("api 호출"), []);
+
+  useEffect(() => console.log('text', keyword), [keyword]);
 
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder='검색' />
       <h1>클릭 횟수: {counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
