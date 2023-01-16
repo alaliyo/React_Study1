c836310d29058285c528c8bd055e285da0df7e3a
alaliyo <des321321@naver.com>
2023-01-16 19:41:21 +0900

    feat: useEffect

--- react-study-start/src/App.js
+++ react-study-start/src/App.js
@@ -1,11 +1,20 @@
-import Button from './Button'
-import styles from './App.module.css'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev+1);
+
+  console.log('계속 실행');
+
+ 
+  useEffect(() => {
+    console.log("api 호출");
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>환영합니다~</h1>
-      <Button text={"메롱"} />
+      <h1>클릭 횟수: {counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
