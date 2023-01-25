3a0a434372941f1bc69b5249a033c67ffa52030d
alaliyo <des321321@naver.com>
2023-01-25 15:04:31 +0900

    feat : Movie style flex 추가

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -11,15 +11,14 @@ function Movies({ id, coverImg, title, summary, genres, num, year}) {
                 <div>
                     <h2 className={styles.movie__title}>
                         <Link to={`/movie/${id}`} target='_blank' >{num}. {title}</Link>
-                    
                     </h2>
+                    <h3 className={styles.movie__year}> {year}</h3>
                     <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+                    <ul className={styles.movie__genres}>genres: &nbsp;
+                        {genres.map(g => <li key={g}>{g}</li>)}
+                    </ul>
                 </div>
             </div>
-            <h3 className={styles.movie__year}> {year}</h3>
-            <ul className={styles.movie__genres}>
-                {genres.map(g => <li key={g}>{g}</li>)}
-            </ul>
         </div>
     );
 }
